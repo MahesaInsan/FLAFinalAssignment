591e9eb2647ede221dbff88745cbacdc5cce646c
Mahesa Insan <mahesainsan@gmail.com>
2023-12-29 21:41:53 +0700

    remove debugging in main

--- main/Main.java
+++ main/Main.java
@@ -1,11 +1,6 @@
 package main;
 
-import database.ProxyProtection;
 import facade.RegistrationService;
-import models.Admin;
-import models.PillMedicine;
-import models.SyrupMedicine;
-import models.User;
 
 public class Main {
 /*  Dimas Dani Zaini - 2502021872
@@ -13,12 +8,7 @@ public class Main {
     Mahesa Insan Raushanfikir - 2501981024
     Naufal Daffa Ryquelme - 2502012312 */
 
-    public static void main(String[] args) {
-        User user = new Admin("", "");
-        ProxyProtection db = new ProxyProtection(user);
-        db.addMedicine(new PillMedicine("P001", "Paracetamol", 200, 20, 13000));
-        db.addMedicine(new SyrupMedicine("S001", "Cough", 100, 3, 27000));
-        
+    public static void main(String[] args) {        
         new RegistrationService();
     }
 }
